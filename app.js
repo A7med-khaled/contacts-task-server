6113d87e2fb462312ab4fb9b955294f96bbc3569
Ahmed Khaled <medolasheen2013@gmail.com>
2020-06-10 02:55:07 +0200

    initiate server

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+
+const PORT = process.env.PORT || 3000;
+
+
+// cors
+app.use(cors());
+
+// body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(morgan('tiny'));
+
+// set port
+app.set('port', PORT);
+
+
+
+const server = require('http').createServer(app);
+server.listen(app.get('port'), function () {
+    console.log(`App Running on Port ${app.get('port')}`);
+});
